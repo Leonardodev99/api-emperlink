f8fa5674fe05f0f9ac93d89dc89526c698c7dd4c
Leonardo Tiago <pequenotiago2016@gmail.com>
2026-03-06 21:28:06 +0100

    Importando os models no index

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,24 @@
 import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../models/User';
+import Comment from '../models/Comment';
+import Follow from '../models/Follow';
+import Group from '../models/Group';
+import GroupMember from '../models/GroupMember';
+import Hashtag from '../models/Hashtag';
+import Message from '../models/Message';
+import Notification from '../models/Notification';
+import Post from '../models/Post';
+import PostHashtag from '../models/PostHashtag';
+import PostImage from '../models/PostImage';
+import Profile from '../models/Profile';
+import Rating from '../models/Rating';
+import Reaction from '../models/Reaction';
+import Report from '../models/Report';
 
 
-const models = [User];
+const models = [User, Comment, Follow, Group, GroupMember,
+  Hashtag, Message, Notification, Post, PostHashtag, PostImage, Profile, Rating, Reaction, Report];
 
 const connection = new Sequelize(databaseConfig);
 
@@ -13,8 +28,10 @@ models.forEach((model) => model.init(connection));
 // Cria associações
 models.forEach((model) => {
   if (model.associate) {
-    model.associate({ User });
+    model.associate({ User, Comment, Follow, Group, GroupMember, Hashtag, Message, Notification,
+      Post,PostHashtag, PostImage, Profile, Rating, Reaction, Report });
   }
 });
 
-export { connection, User };
+export { connection, User, Comment, Follow, Group, GroupMember, Hashtag, Message, Notification,
+  Post, PostHashtag, PostImage, Profile, Rating,Reaction, Report };
